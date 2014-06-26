f8716b81c5d24e38c0992966e6c3b9b688bcbc32
Victor Chen <VC@Victors-MacBook-Pro.local>
2014-06-25 22:01:44 -0700

    Initial Commit

---
+++ Multi_Thread_Prime/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Multi_Thread_Prime
+//
+//  Created by Victor Chen on 6/25/14.
+//  Copyright (c) 2014 Victor Chen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
